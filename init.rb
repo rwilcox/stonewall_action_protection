3c02b4f4a2fdbb65a2eae9c5f4ceba0524477748
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-03-27 10:58:26 +0000

    Got plugin up and loading the code

--- init.rb
+++ init.rb
@@ -1,1 +1,3 @@
 # Include hook code here
+#
+require File.dirname(__FILE__) + '/lib/stonewall_action_protection'
--- lib/stonewall_action_protection.rb
+++ lib/stonewall_action_protection.rb
@@ -1,1 +1,7 @@
 # StonewallActionProtection
+
+require  'action_controller'
+
+ActionController::Base.class_eval do
+  include StandardPermissions
+end
